4bc07cd49ed2dd9e304741df3202eba8cfffe3a4
Alex Ho <mralexho@gmail.com>
2021-04-12 12:55:57 -0400

    add a slug to our Company model

--- solarpv/myapp/models.py
+++ solarpv/myapp/models.py
@@ -1,18 +1,38 @@
 from django.db import models
+from django.utils import text
 
 
 # Create your models here.
 
 class Appellation(models.Model):
-    title = models.CharField(max_length=255)
+    title = models.CharField(max_length=255, unique=True)
+
+    class Meta:
+        ordering = ['title']
+
+        def __init__(self):
+            self.title = None
+
+        def __str__(self):
+            return self.title
 
 
 class Company(models.Model):
     name = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=200, unique=True, null=True)
+
+    class Meta:
+        ordering = ['name']
+
+        def __init__(self):
+            self.name = None
+
+        def __str__(self):
+            return self.name
 
 
 class User(models.Model):
-    username = models.CharField(max_length=255)
+    username = models.CharField(max_length=255, unique=True)
     company = models.ForeignKey(Company, on_delete=models.CASCADE)
     appellation = models.ForeignKey(Appellation, on_delete=models.CASCADE)
     first_name = models.CharField(max_length=255)
@@ -22,3 +42,12 @@ class User(models.Model):
     email = models.CharField(max_length=255)
     office_phone = models.CharField(max_length=255)
     cell_phone = models.CharField(max_length=255)
+
+    class Meta:
+        ordering = ['username']
+
+        def __init__(self):
+            self.username = None
+
+        def __str__(self):
+            return self.username
