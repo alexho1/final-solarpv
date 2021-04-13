2d95b0bd09a4e2f5eade69d1c0c14b173692ae06
Alex Ho <mralexho@gmail.com>
2021-04-12 22:18:05 -0400

    register admin models

--- solarpv/myapp/admin.py
+++ solarpv/myapp/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import User, Certificate, Company
+
 
 # Register your models here.
+class UserAdmin(admin.ModelAdmin):
+    exclude = ()
+
+
+class CertificateAdmin(admin.ModelAdmin):
+    exclude = ()
+
+
+class CompanyAdmin(admin.ModelAdmin):
+    exclude = ()
+
+
+admin.site.register(User, UserAdmin)
+admin.site.register(Certificate, UserAdmin)
+admin.site.register(Company, UserAdmin)
